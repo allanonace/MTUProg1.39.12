72788cefc485d58cbf4c6137fe4f2222b986576f
m.regil <m.regil@bizintekinnova.com>
2019-02-20 12:14:58 +0100

    Change file ValidatorBehavior.cs

--- aclara_meters/Behaviors/ValidatorBehavior.cs
+++ aclara_meters/Behaviors/ValidatorBehavior.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 using Xamarin.Forms;
 
@@ -50,6 +51,83 @@ namespace aclara_meters.Behaviors
         {
             bindable.TextChanged -= HandleTextChanged;
             base.OnDetachingFrom(bindable);
+        }
+    }
+    public class CommentsLengthValidatorBehavior : Behavior<BorderlessEntry>
+    {
+        public int MaxLength { get; set; }
+
+        protected override void OnAttachedTo(BorderlessEntry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += OnEntryTextChanged;
+        }
+
+        protected override void OnDetachingFrom(BorderlessEntry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= OnEntryTextChanged;
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var entry = (BorderlessEntry)sender;
+
+
+            if (!string.IsNullOrWhiteSpace(e.NewTextValue))
+            {
+
+                if (entry.Text != null && entry.Text.Length > this.MaxLength)
+                {
+                    string entryText = entry.Text;
+                    entry.TextChanged -= OnEntryTextChanged;
+                    entry.Text = e.OldTextValue;
+                    entry.TextChanged += OnEntryTextChanged;
+                }
+
+            }
+
+        }
+    }
+    public class EntryLengthValidatorBehavior : Behavior<BorderlessEntry>
+    {
+        public int MaxLength { get; set; }
+
+        protected override void OnAttachedTo(BorderlessEntry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += OnEntryTextChanged;
+        }
+
+        protected override void OnDetachingFrom(BorderlessEntry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= OnEntryTextChanged;
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var entry = (BorderlessEntry)sender;
+
+
+            if (!string.IsNullOrWhiteSpace(e.NewTextValue))
+            {
+                bool isValid = e.NewTextValue.ToCharArray().All(x => char.IsDigit(x)); //Make sure all characters are numbers
+
+                ((BorderlessEntry)sender).Text = isValid ? e.NewTextValue : e.NewTextValue.Remove(e.NewTextValue.Length - 1);
+
+                if (isValid)
+                {
+                    if (entry.Text != null && entry.Text.Length > this.MaxLength)
+                    {
+                        string entryText = entry.Text;
+                        entry.TextChanged -= OnEntryTextChanged;
+                        entry.Text = e.OldTextValue;
+                        entry.TextChanged += OnEntryTextChanged;
+                    }
+                }
+            }
+
         }
     }
 }
